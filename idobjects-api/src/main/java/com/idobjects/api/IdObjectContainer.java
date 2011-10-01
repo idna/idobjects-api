128afc64b9a332d56c98448b6fa598bb9126f4d7
blackforest <hakan.kocabas@gmx.de>
2011-10-01 13:18:26 +0200

    Test Lokal

--- idobjects-api/src/main/java/com/idobjects/api/IdObjectContainer.java
+++ idobjects-api/src/main/java/com/idobjects/api/IdObjectContainer.java
@@ -19,6 +19,8 @@ public interface IdObjectContainer{
     
     void changeId( ObjectIdentifier oldId, ObjectIdentifier newId, IdObject idObject);
     
+    
+    
     Collection<IdObject> elements();
 
 }
